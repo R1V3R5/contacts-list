9c2f4d039dc10d446ecf412d5631e88b3da9685d
Joshua Rivers <joshua.rivers@gmail.com>
2016-07-18 17:15:43 -0400

    have contacts adding to list and display none if clicked.

--- src/js/controllers/app-controller.js
+++ src/js/controllers/app-controller.js
@@ -1,4 +1,6 @@
-import { Contact } from '../models/contact'
+import _ from 'lodash';
+import $ from 'jquery';
+import { Contact } from '../models/contact';
 class AppController {
 
   constructor (form, container) {
@@ -7,6 +9,7 @@ class AppController {
   }
   
   init () {
+    this.contactClick();
     this.formSubmit();
 
   }
@@ -23,6 +26,7 @@ class AppController {
       event.preventDefault();
       // console.log(event);
     let newContact = {};
+      newContact.picture = 'http://placecage.com/75/75'; 
       newContact.firstName = this.form.find('.firstName').val();
       newContact.lastName = this.form.find('.lastName').val();
       newContact.phoneNumber = this.form.find('.phoneNumber').val();
@@ -31,20 +35,39 @@ class AppController {
       newContact.id = _.random(100,999);
 
       this.addContact(newContact)
-      // console.log(newContact)
+      console.log(newContact)
     });
   }
 
+  contactClick () {
+    this.container.on('click', '.contact', (event) => {
+      event.preventDefault();
+      console.log(event.currentTarget.dataset.contactid)
+      
+      let id = event.currentTarget.dataset.contactid
+      // let contact = _.find(this.contact, { id: id });
+      // console.log(this)
+      // contact.toggleVisible();
+
+      $(event.currentTarget).toggleClass('delete');
+
+      // console.log(task);
+
+    });
+  }  
+
   contactTmpl (contact) {
     return `
-      <div class="contact" data-contactId="${contact.id}">
-        <div class="contactFullName">${contact.firstName} ${contact.lastName}</div>
-        <div class="contactPhoneNumber">${contact.phoneNumber}</div>
-        <div class="location">${contact.city}, ${contact.state}</div>
+      <div class="contact" data-contactid="${contact.id}">
+        <div class="picture"><img src="${contact.picture}"></div>
+        <div class="details">
+          <div class="contactFullName">${contact.firstName} ${contact.lastName}</div>
+          <div class="contactPhoneNumber">${contact.phoneNumber}</div>
+          <div class="location">${contact.city}, ${contact.state}</div>
+        </div>
       </div>
     `
-
-  }
+  }  
 
 }
 
--- src/js/models/contact.js
+++ src/js/models/contact.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 
+
+
 class Contact {
 
   constructor (options) {
@@ -8,8 +10,15 @@ class Contact {
     this.phoneNumber  = options.phoneNumber;
     this.city         = options.city;
     this.state        = options.state;
-    // this.id           = options.id; 
+    this.id           = options.id;
+    this.visible      = options.visible || true;
+    this.picture      = options.picture || 'http://placecage.com/75/75'; 
   }
+
+  toggleVisible () {
+    return this.visible = !this.visible;
+  }
+
 }
 
 export { Contact };
